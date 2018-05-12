f8c8dc72b36564b5047a32abbb5091b7ff50ccae
BohdanRomaniuk <bohdan2307@gmail.com>
2018-05-12 15:48:51 +0300

    Perform search by genres

--- website/website/Controllers/SearchController.cs
+++ website/website/Controllers/SearchController.cs
@@ -18,13 +18,29 @@ namespace website.Controllers
             db = _db;
         }
 
-        public IActionResult Query(int country=1, string movieName="", int page = 1)
+        public IActionResult Query(int country=0, string movieName="", int[] genres=null, int page = 1)
         {
-            IQueryable<Movie> allMovies = db.Movies
+            /*IQueryable<Movie> allMovies = db.Movies
                                           .Include(m => m.Countries).ThenInclude(c => c.Country)
                                           .Include(m => m.Genres).ThenInclude(g => g.Genre)
-                                          .Where(c => c.Countries.Any(p => p.Country.Id==country))
-                                          .Where(c=>c.Name.Contains(movieName));
+                                          .Where(m => m.Countries.Any(p => p.Country.Id == country))
+                                          .Where(m => m.Name.Contains(movieName))
+                                          .Where(m => m.Genres.Where(g=>genres.Contains(g.Genre.Id)).Count()!=0);*/
+            IQueryable<Movie> allMovies = db.Movies
+                                          .Include(m => m.Countries).ThenInclude(c => c.Country)
+                                          .Include(m => m.Genres).ThenInclude(g => g.Genre);
+            if(country!=0)
+            {
+                allMovies = allMovies.Where(m => m.Countries.Any(p => p.Country.Id == country));
+            }
+            if(movieName!="")
+            {
+                allMovies = allMovies.Where(m => m.Name.Contains(movieName));
+            }
+            if(genres!=null && genres.Length!=0)
+            {
+                allMovies = allMovies.Where(m => m.Genres.Where(g => genres.Contains(g.Genre.Id)).Count() != 0);
+            }
             int perPage = 20;
             return View(new SearchViewModel(db.Genres, db.Countries, allMovies.Skip(page*perPage-perPage).Take(perPage), country, movieName));
         }
